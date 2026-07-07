d80e143a5848bb84ade0bd76c97b143a3c86d9b7
sugiharto <sugihartobangsawan@gmail.com>
2026-07-07 20:24:07 +0700

    adding flow chart and update main.py

--- main.py
+++ main.py
@@ -1,9 +1,37 @@
 from dotenv import load_dotenv
 import os
+from langchain_core.messages import HumanMessage
+from langgraph.graph import StateGraph,MessagesState, END
+
+from nodes import run_agent_reasoning, tool_node
 
 load_dotenv()
 
+AGENT_REASON="agent_reason"
+ACT="act"
+LAST=-1
+
+def should_continue(state: MessagesState) -> str:
+    if not state['messages'][LAST].tool_calls:
+        return END
+    return ACT
+
+
+flow = StateGraph(MessagesState)
+
+flow.add_node(AGENT_REASON, run_agent_reasoning)
+flow.set_entry_point(AGENT_REASON)
+flow.add_node(ACT, tool_node)
+
+flow.add_conditional_edges(AGENT_REASON, should_continue,{
+    END:END,
+    ACT:ACT
+})
+
+flow.add_edge(ACT, AGENT_REASON)
+
+app = flow.compile()
+app.get_graph().draw_mermaid_png(output_file_path='flow.png')
 
 if __name__ == '__main__':
     print("Hello React LangGraph  Function Calling")
-    print(os.getenv("GOOGLE_API_KEY"))
