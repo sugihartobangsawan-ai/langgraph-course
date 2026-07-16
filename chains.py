523d1aea7d39e58d5e8db3232f03908288e19d9c
sugiharto <sugihartobangsawan@gmail.com>
2026-07-16 10:11:16 +0700

    adding tool executor

--- chains.py
+++ chains.py
@@ -13,7 +13,7 @@ from langchain_core.messages import HumanMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_google_genai import ChatGoogleGenerativeAI
 
-from schema import AnswerQuestion
+from schema import AnswerQuestion, ReviseAnswer
 
 llm = ChatGoogleGenerativeAI(model="gemini-3.1-flash-lite")
 parser = JsonOutputToolsParser(return_id=True)
@@ -48,6 +48,20 @@ first_responder_prompt_template = actor_prompt_template.partial(
 first_responder = first_responder_prompt_template | llm.bind_tools(
     tools=[AnswerQuestion], tool_choice="AnswerQuestion"
 )
+
+revise_instructions = """
+Revise your previous answer using the new information.
+    - You should use the previous critique to add important information to your answer.
+    - You must include numerical citations in your revised answer to ensure it can be verfied.
+    - Add a 'references' section to the bottom of your answer (which does not count towards the word limit)
+        - [1] https://example.com
+        - [2] https://example.com
+    - You should use the previous critique to remove superfluous information from your answer and make SURE it is not more than 250 words.
+"""
+
+revisor = actor_prompt_template.partial(
+    first_instruction=revise_instructions
+) | llm.bind_tools(tols=[ReviseAnswer],tool_choice="ReviseAnswer")
 
 if __name__ == "__main__":
     human_message = HumanMessage(
---
+++ tool_executor.py
@@ -0,0 +1,26 @@
+from typing import List
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain_tavily import TavilySearch
+from langchain_core.tools import StructuredTool
+from langgraph.prebuilt import ToolNode
+
+from schema import AnswerQuestion, ReviseAnswer
+
+tavily_tool = TavilySearch(max_results=5)
+
+def run_queries(search_queries: List[str], **kwargs):
+    """Run the generated queries."""
+    return tavily_tool.batch(
+        [{'query': query} for query in search_queries]
+    )
+
+execute_tools = ToolNode(
+    [
+        StructuredTool.from_function(run_queries, name=AnswerQuestion.__name__),
+        StructuredTool.from_function(run_queries, name=ReviseAnswer.__name__),
+    ]
+)
