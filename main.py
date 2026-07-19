7f36a43a1e10e38ccec5a1a48562ddc9855ffbe2
sugiharto <sugihartobangsawan@gmail.com>
2026-07-19 09:48:03 +0700

    Project-Structure

---
+++ main.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
