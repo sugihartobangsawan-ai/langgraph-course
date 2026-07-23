c7e216345649a3a479f83ba4d8037a28584755cd
sugiharto <sugihartobangsawan@gmail.com>
2026-07-23 09:29:21 +0700

    retrieval_grader and grade_documents

--- graph/chains/tests/test_chains.py
+++ graph/chains/tests/test_chains.py
@@ -1,2 +1,17 @@
-def test_foo() -> None:
-    assert 1 == 1
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from graph.chains.retrieval_grader import GradeDocuments,retrieval_grader
+from ingestion import retriever
+
+def test_retrieval_grader_answer_no() -> None:
+    question = "why prabowo stupid"
+    docs = retriever.invoke(question)
+    doc_txt = docs[1].page_content
+
+    res: GradeDocuments = retrieval_grader.invoke(
+        {"question": question, "document": doc_txt}
+    )
+
+    assert res.binary_score == 'no'
